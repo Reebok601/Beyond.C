52475090bac0d9772bfee772dcaf5305e53dd993
Ribak Dc <ribakdc@Ribaks-MacBook-Pro.local>
2022-07-22 20:09:34 +1000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+
+import './App.css';
+import Cart from './pages/Cart';
+
+
+import Home from "./pages/Home";
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+const App = () => {
+  return <Home/>;
+};
+
+export default App
